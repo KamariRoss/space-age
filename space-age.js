323ead59df202c21d7672689daf3371af5b895bc
KamariRoss <kamarimobile@gmail.com>
2020-06-30 12:30:57 -0400

    Turned the planets into objects based on critiqued

--- space-age.js
+++ space-age.js
@@ -19,35 +19,54 @@
 // Return the age
 
 export const age = (planets, seconds) => {
-  const oneEarthYear = seconds / 31557600
   let planetAge = 0
+  const planetaryEarthYearConversion = {
+    mercury: 0.2408467,
+    earth: seconds / 31557600,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  }
   switch (planets) {
     case "earth":
-      planetAge = oneEarthYear
+      planetAge = planetaryEarthYearConversion.earth
       break
     case "mercury":
-      planetAge = oneEarthYear / 0.2408467
+      planetAge =
+        planetaryEarthYearConversion.earth /
+        planetaryEarthYearConversion.mercury
       break
     case "venus":
-      planetAge = oneEarthYear / 0.61519726
+      planetAge =
+        planetaryEarthYearConversion.earth / planetaryEarthYearConversion.venus
       break
     case "mars":
-      planetAge = oneEarthYear / 1.8808158
+      planetAge =
+        planetaryEarthYearConversion.earth / planetaryEarthYearConversion.mars
       break
     case "jupiter":
-      planetAge = oneEarthYear / 11.862615
+      planetAge =
+        planetaryEarthYearConversion.earth /
+        planetaryEarthYearConversion.jupiter
       break
     case "saturn":
-      planetAge = oneEarthYear / 29.447498
+      planetAge =
+        planetaryEarthYearConversion.earth / planetaryEarthYearConversion.saturn
       break
     case "uranus":
-      planetAge = oneEarthYear / 84.016846
+      planetAge =
+        planetaryEarthYearConversion.earth / planetaryEarthYearConversion.uranus
       break
     case "neptune":
-      planetAge = oneEarthYear / 164.79132
+      planetAge =
+        planetaryEarthYearConversion.earth /
+        planetaryEarthYearConversion.neptune
       break
   }
-  return parseFloat(planetAge.toFixed(2))
+  return Number(planetAge.toFixed(2))
 }
 
 // const seconds = process.argv[3]
